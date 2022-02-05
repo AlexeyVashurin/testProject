480059ff394606d4fcd80fd211ae75819f7c5149
AlexeyVashurin <a.s.vashurin@gmail.com>
2022-02-05 21:57:13 +0300

    update

---
+++ Assets/Skripts/MirrorCoefficient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class MirrorCoefficient : MonoBehaviour
+{
+    public float Coefficient = 10;
+
+    public bool checkHitRaycast;
+    
+
+    public delegate void hitTouch(int coof);
+
+    public event hitTouch hitEvent;
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    public void hitObject()
+    {
+        hitEvent?.Invoke(10);
+    }
+
+
+
+    // Update is called once per frame
+    
+}
