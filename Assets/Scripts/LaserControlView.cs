d0add743ae05ea9b35ccc08af8d9061c6602fcd1
AlexeyVashurin <a.s.vashurin@gmail.com>
2022-09-06 00:57:14 +0400

    Refactoring

---
+++ Assets/Scripts/LaserControlView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserControlView : MonoBehaviour
+{
+    [SerializeField]private Button rotateLeftPlatformButton, rotateRightPlatformButton, moveUpButton, moveDownButton;
+
+    public void Subscribe(Action moveLeft, Action moveRight, Action moveUp, Action moveDown)
+    {
+        rotateLeftPlatformButton.onClick.AddListener(moveLeft.Invoke);
+        rotateRightPlatformButton.onClick.AddListener(moveRight.Invoke);
+        moveUpButton.onClick.AddListener(moveUp.Invoke);
+        moveDownButton.onClick.AddListener(moveDown.Invoke);
+    }
+
+}
---
+++ Assets/Scripts/LaserMoveController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LaserMoveController : MonoBehaviour
+{
+    [SerializeField]private GameObject _gun, _platform;
+    
+    
+
+    [SerializeField] private LaserControlView _laserControlView;
+
+    void Start()
+    {
+        _laserControlView.Subscribe(MoveLeft,MoveRight,MoveUp,MoveDown);
+    }
+
+    private void MoveRight()
+    {
+        _platform.transform.Rotate(new Vector3(0, +5, 0));
+    }
+
+    private void MoveLeft()
+    {
+        _platform.transform.Rotate(new Vector3(0, -5, 0));
+    }
+
+    private void MoveDown()
+    {
+        _gun.transform.Rotate(new Vector3(+5, 0, 0));
+    }
+
+    private void MoveUp()
+    {
+        _gun.transform.Rotate(new Vector3(-5, 0, 0));
+    }
+}
